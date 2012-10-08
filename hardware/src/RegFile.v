7c5c20b8c13a5f303a347cbe9f2ff4e8ffeaed00
Oleksiy_G_Budilovsky <cs150-rb@p380-20.EECS.Berkeley.EDU>
2012-10-08 13:46:41 -0700

    reg file ready, to be tested

--- hardware/src/RegFile.v
+++ hardware/src/RegFile.v
@@ -21,5 +21,18 @@ module RegFile(input clk,
                output [31:0] rd1, rd2);
 
 // Implement your register file here, then delete this comment.
+	parameter ENTRIES = 32;
+	reg [31:0] registers [ENTRIES-1:0];	
+
+	always @ (ra1, ra2, wd) begin
+		rd1 = registers[ra1];
+		rd2 = registers[ra2];
+	end
+	
+	always @ (posedge clk) begin
+		if (we && wa != 5'b0) begin
+			registers[wa] <= wd;
+		end
+	end	
 
 endmodule
