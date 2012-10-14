d9c0b8d9e996656d15aa803f44cd7523fff2d114
Ashwin_Marathe <cs150-cx@p380-11.EECS.Berkeley.EDU>
2012-10-13 17:50:09 -0700

    MIPS 150 modified

--- hardware/src/MIPS150.v
+++ hardware/src/MIPS150.v
@@ -13,13 +13,13 @@ wire we_reg, AequalsB;
 
 
 //Instantiate all wires first
-wire RegDest_X, RegDest_Y, ALURegSel_X,ALURegSel_Y,RegWrite_X, RegWrite_Y, RegWrite_Z, we_reg, MemToReg_X,MemToReg_Y,MemToReg_Z, inst_X, inst_Y, DataOutValid, DataInReady, DataInValid, DataOutReady, PCPlus8_X, PCPlus8_Y,
-wire [1:0] JBout, JALCtrl_X, JALCtrl_Y                                              
-wire [2:0] ALUSrcB_X, ALUSrcB_Y,LdStCtrl_X,LdStCtrl_Y,LdStCtrl_Z,
-wire [3:0] ALUCtrl_X, ALUCtrl_Y, JumpBranch_X, JumpBranch_Y, we_e, we_d,
-wire [31:0] PC_X, PCout_X, PCout_Y, PCoutplus4_X, PCoutplus4_Y, PCoutplus4_Z, PC_shifted_Y, RS, RT, rd1,rd2,wd,RT,ALU_out_Y,ALU_out_Z, wd_y, wd_z, RT_shifted, UARTout, MemUARTout
-wire [7:0] UARTwrite, UARTread
-wire [11:0] mem_adr
+reg RegDest_X, RegDest_Y, ALURegSel_X,ALURegSel_Y,RegWrite_X, RegWrite_Y, RegWrite_Z, MemToReg_X,MemToReg_Y,MemToReg_Z, inst_X, inst_Y, DataOutValid, DataInReady, DataInValid, DataOutReady, PCPlus8_X, PCPlus8_Y, dmemout
+reg [1:0] JBout, JALCtrl_X, JALCtrl_Y                                              
+reg [2:0] ALUSrcB_X, ALUSrcB_Y,LdStCtrl_X,LdStCtrl_Y,LdStCtrl_Z,
+reg [3:0] ALUCtrl_X, ALUCtrl_Y, JumpBranch_X, JumpBranch_Y, we_e, we_d,
+reg [31:0] PC_X, PCout_X, PCout_Y, PCoutplus4_X, PCoutplus4_Y, PCoutplus4_Z, PC_shifted_Y, RS, RT, rd1,rd2,wd,RT,ALU_out_Y,ALU_out_Z, wd_y, wd_z, RT_shifted, UARTout, MemUARTout, PC_in;
+reg [7:0] UARTwrite, UARTread
+reg [11:0] mem_adr
 RegFile RegFile(                            
             .clk(clk),                       
             .we(we_reg),
@@ -90,5 +90,38 @@ ControlUnit ControlUnit(
 	.LdStCtrl(LdStCtrl_X),
 	.JumpBranch(JumpBranch_X)
 );
+
+always @(posedge clk)begin
+	RegDest_Y <= RegDest_X;
+	ALURegSel_Y <= ALURegSel_X;
+	RegWrite_Y <= RegWrite_X;
+	RegWrite_Z <= RegWrite_Y;
+	MemToReg_Y <= MemToReg_X;
+	MemToReg_Z <= MemToReg_Y;
+	inst_Y <= inst_X;
+	JALCtrl_Y <= JALCtrl_X;
+	ALUSrcB_Y <= ALUSrcB_X;
+	LdStCtrl_Y <= LdStCtrl_X;
+	LdStCtrl_Z <= LdStCtrl_Y;
+	ALUCtrl_X <= ALUCtrl_Y;
+	JumpBranch_Y <= JumpBranch_X;
+	ALU_out_Z <= ALU_out_Y;
+	wd_z <= wd_y;
+
+	ALU_SrcB_Y <= ALU_SrcB_X;
+	LdStCtrl_Y <= LdStCtrl_X;
+	LdStCtrl_Z <= LdStCtrl_Y;
+	ALUCtrl_Y <= ALUCtrl_X;
+	JumpBranch_Y <= JumpBranch_X;
+	PCout_Y <= PCout_X;
+	PCoutplus4_Y <= PCoutplus4_X;
+	PCoutplus4_Z <= PCoutplus4_Y;
+	ALU_out_Z <= ALU_out_Y;
+	wd_z <= wd_z;
+	
+	
+	PCout <= PC_X;
+
+end
   
 endmodule
