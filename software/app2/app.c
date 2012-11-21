273be3455c19668b26f193c3444800fd1391e1df
Ashwin_Marathe <cs150-cx@p380-11.EECS.Berkeley.EDU>
2012-11-20 21:32:31 -0800

    R,v,V print out respective letters

--- software/app2/app.c
+++ software/app2/app.c
@@ -4,6 +4,10 @@
 void send_time(char c);
 void send_byte(char b);
 void r100M();
+void R100M();
+void v100M();
+void V100M();
+void addone();
 
 int main(){
 
@@ -46,33 +50,15 @@ int tstart, tend;
 
 while(1)  {
 	if (STATE == 0x72) {
-			//initial time
-			//asm("li $t4, 0x1f0000bc");
-			//asm("lw $t5, 0($t4)"); //total seconds start
 			asm("li $t0, 0x1f0000c8"); //timer, tx, rx and global
 			asm("li $t1, 0x1"); //timer, tx, rx and global
-			//asm("sw $t1, 0($t0)");
 			r100M();
 			asm("li $t0, 0x1f0000c8"); //timer, tx, rx and global
 			asm("li $t1, 0x1"); //timer, tx, rx and global
-			//asm("sw $t1, 0($t0)");
 			asm("li $t0, 0x8c01"); //timer, tx, rx and global
-			//asm("mtc0 $t0, $12");
 			asm("nop");
 			asm("nop");
 			asm("nop");
-//			asm("add $t5, $0, $0");
-//			asm("addi  $t6,$0, 0x600"); //100M
-			//asm("loop:");
-			//asm("addi $t5, $t5, 0x1");
-			//asm("slt $t7, $t6, $t5");
-			//asm("beq $t7, $0, loop");
-			//asm("bne $0, $0, loop");
-			//ending time
-			//asm("lw $t6, 0($t4)"); //total seconds end 
-			//asm("subu $t6, $t6, $t5");
-			//asm("li $t5, 0x1f0000a8");
-			//asm("sw $t6, 0($t5)"); 
 			send_time('r');
 asm("li $t5, 0x8c01"); //timer, tx, rx and global
 		asm("mtc0 $t5, $12");
@@ -82,33 +68,69 @@ asm("li $t5, 0x8c01"); //timer, tx, rx and global
 			asm("li $t0, 0x1f0000c0");
 			asm("sw $0, 0($t0)");
 	}
-	asm("add $t4, $t4, $t4");
-	/*
-	switch (STATE) {
-		case 0x72: //r
-			//initial time
-			//asm("li $t4, 0x1f0000bc");
-			//asm("lw $t5, 0($t4)"); //total seconds start
-			//r100M();
-			asm("add $t5, $0, $0");
-			asm("addi  $t6,$0, 0x600"); //100M
-			asm("loop:");
-			asm("addi $t5, $t5, 0x1");
-			asm("slt $t7, $t6, $t5");
-			asm("beq $t7, $0, loop");
-			//ending time
-			//asm("lw $t6, 0($t4)"); //total seconds end 
-			//asm("subu $t6, $t6, $t5");
-			//asm("li $t5, 0x1f0000a8");
-			//asm("sw $t6, 0($t5)"); 
-			send_time('r');
-			//clears the STATE var
+	else if (STATE == 0x52){
+			asm("li $t0, 0x1f0000c8"); //timer, tx, rx and global
+			asm("li $t1, 0x1"); //timer, tx, rx and global
+			R100M();
+			asm("li $t0, 0x1f0000c8"); //timer, tx, rx and global
+			asm("li $t1, 0x1"); //timer, tx, rx and global
+			asm("li $t0, 0x8c01"); //timer, tx, rx and global
+			asm("nop");
+			asm("nop");
+			asm("nop");
+			send_time('R');
+asm("li $t5, 0x8c01"); //timer, tx, rx and global
+		asm("mtc0 $t5, $12");
+		asm("nop");
+		
+	//clears the STATE var
 			asm("li $t0, 0x1f0000c0");
 			asm("sw $0, 0($t0)");
-		break;
-	}
-	*/
+	
+}
+	else if (STATE == 0x76){
+			asm("li $t0, 0x1f0000c8"); //timer, tx, rx and global
+			asm("li $t1, 0x1"); //timer, tx, rx and global
+			v100M();
+			asm("li $t0, 0x1f0000c8"); //timer, tx, rx and global
+			asm("li $t1, 0x1"); //timer, tx, rx and global
+			asm("li $t0, 0x8c01"); //timer, tx, rx and global
+			asm("nop");
+			asm("nop");
+			asm("nop");
+			send_time('v');
+asm("li $t5, 0x8c01"); //timer, tx, rx and global
+		asm("mtc0 $t5, $12");
+		asm("nop");
+		
+	//clears the STATE var
+			asm("li $t0, 0x1f0000c0");
+			asm("sw $0, 0($t0)");
+	
+}
+	else if (STATE == 0x56){
+			asm("li $t0, 0x1f0000c8"); //timer, tx, rx and global
+			asm("li $t1, 0x1"); //timer, tx, rx and global
+			V100M();
+			asm("li $t0, 0x1f0000c8"); //timer, tx, rx and global
+			asm("li $t1, 0x1"); //timer, tx, rx and global
+			asm("li $t0, 0x8c01"); //timer, tx, rx and global
+			asm("nop");
+			asm("nop");
+			asm("nop");
+			send_time('V');
+asm("li $t5, 0x8c01"); //timer, tx, rx and global
+		asm("mtc0 $t5, $12");
+		asm("nop");
+		
+	//clears the STATE var
+			asm("li $t0, 0x1f0000c0");
+			asm("sw $0, 0($t0)");
+	
+}
+	asm("add $t4, $t4, $t4");
 };
+
 	return 0;
 }
 
@@ -121,6 +143,55 @@ void r100M() {
 		//send_time('a');
 		asm("addiu $t7, $t7, 0x1");
 		asm("bne $t7, $t8, loop");
+		asm("nop");
+		//asm("addiu $t6, $t6, 0x1");
+}
+void R100M() {
+		asm("add $t7, $0, $0");
+		asm("lui  $t8, 0x05f5"); //100M
+		asm("ori $t8, $t8, 0xe100");
+		//asm("li $t8, 0x05f5e100");
+		asm("loopR:");
+		//send_time('a');
+		addone();
+		asm("bne $t7, $t8, loopR");
+		asm("nop");
+		//asm("addiu $t6, $t6, 0x1");
+}
+void addone(){
+		asm("addiu $t7, $t7, 0x1");
+}
+void v100M() {
+		asm("add $t7, $0, $0");
+		asm("lui  $t8, 0x05f5"); //100M
+		asm("ori $t8, $t8, 0xe100");
+		//asm("li $t8, 0x05f5e100");
+		asm("lui $t6, 0x1f00");
+		asm("ori $t6, $t6, 0x00ac");
+		asm("loopv:");
+		//send_time('a');
+		asm("lw $t7, 0($t6)");
+		asm("nop");
+		asm("addiu $t7, $t7, 0x1");
+		asm("sw $t7, 0($t6)");
+		asm("bne $t7, $t8, loopv");
+		asm("nop");
+		//asm("addiu $t6, $t6, 0x1");
+}
+void V100M() {
+		asm("add $t7, $0, $0");
+		asm("lui  $t8, 0x05f5"); //100M
+		asm("ori $t8, $t8, 0xe100");
+		//asm("li $t8, 0x05f5e100");
+		asm("lui $t6, 0x1f00");
+		asm("ori $t6, $t6, 0x00ac");
+		asm("loopV:");
+		asm("lw $t7, 0($t6)");
+		asm("nop");
+		//send_time('a');
+		addone();
+		asm("sw $t7, 0($t6)");
+		asm("bne $t7, $t8, loopV");
 		asm("nop");
 		//asm("addiu $t6, $t6, 0x1");
 }
