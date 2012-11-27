7e81b0b70ac5d66223befa823b9abd0516ea8183
Alex Budilovsky <alexbudy@gmail.com>
2012-11-26 21:13:58 -0800

    LE helper implemented

--- hardware/src/LineEngine.v
+++ hardware/src/LineEngine.v
@@ -31,6 +31,21 @@ module LineEngine(
 
     // Implement Bresenham's line drawing algorithm here!
 
+	bres_helper helper(
+		.x0(x0),
+		.y0(y0),
+		.x1(x1),
+		.y1(y1),
+		.newx0(newx0),
+		.newy0(newy0),
+		.newx1(newx1),
+		.newy1(newy1),
+		.steep(steep),
+		.deltax(deltax),	
+		.deltay(deltay)	
+	);
+	
+	
 
    
     // Remove these when you implement this module:
@@ -38,4 +53,30 @@ module LineEngine(
     assign wdf_wr_en = 1'b0;
     assign LE_ready = 1'b1;
 
+endmodule
+
+module bres_helper(
+	input [9:0] 	x0,
+	input [9:0] 	x1,
+	input [9:0] 	y0,
+	input [9:0] 	y1,
+	output [9:0]	newx0,
+	output [9:0]	newx1,
+	output [9:0]	newy0,
+	output [9:0]	newy1,
+	output 			steep,
+	output [9:0]	deltax,
+	output [9:0]	deltay
+);
+
+	assign steep = ((y1>y0) ? (y1 - y0) : (y0 - y1)) > ((x1>x0)?(x1-x0):(x0-x1));
+	
+	assign newx0 = steep ? ((y0 > y1) ? y1 : y0) : ((x0 > x1) ? x1 : x0);	
+	assign newx1 = steep ? ((y0 > y1) ? y0 : y1) : ((x0 > x1) ? x0 : x1);	
+	assign newy0 = steep ? ((y0 > y1) ? x1 : x0) : ((x0 > x1) ? y1 : y0);	
+	assign newy1 = steep ? ((y0 > y1) ? x0 : x1) : ((x0 > x1) ? y0 : y1);	
+	
+	assign deltax = newx1 - newx0; 
+	assign deltay = (newy1>newy0) ? (newy1-newy0) : (newy0-newy1);
+	
 endmodule
